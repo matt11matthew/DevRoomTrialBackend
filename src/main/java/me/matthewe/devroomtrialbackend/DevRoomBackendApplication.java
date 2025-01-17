d68dddab20d60cb4540aed3118f36dfb5da0330f
matt11matthew <matt11matthew@gmail.com>
2025-01-17 17:16:08 -0500

    1

--- src/main/java/me/matthewe/devroomtrialbackend/DevRoomBackendApplication.java
+++ src/main/java/me/matthewe/devroomtrialbackend/DevRoomBackendApplication.java
@@ -7,10 +7,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "me.matthewe.devroomtrialbackend")
 @EnableJpaRepositories(basePackages = "me.matthewe.devroomtrialbackend.repository")
-@EntityScan(basePackages = "me.matthewe.premiertrialbackend.data")
-public class PremierTrialBackendApplication {
+@EntityScan(basePackages = "me.matthewe.devroomtrialbackend.data")
+public class DevRoomBackendApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PremierTrialBackendApplication.class, args);
+        SpringApplication.run(DevRoomBackendApplication.class, args);
     }
 
 
